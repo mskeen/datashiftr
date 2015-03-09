fa2df349bceddcfd2691dc125256c8131c4e88d2
Mike Skeen <skeen.mike@gmail.com>
2015-03-09 16:25:40 -0400

    Add function to retrieve a datafile's content

--- R/datafiles_functions.R
+++ R/datafiles_functions.R
@@ -4,3 +4,10 @@ datafiles_list <- function(api_key) {
   df_list <- do.call("rbind", df_list)
   return(df_list)
 }
+
+datafile_content <- function(api_key, datafile_id) {
+  content_json <- RCurl::getURL(paste0("https://datashift.ca/api/v1/datafiles/", datafile_id, ".json?api_key=", api_key))
+  content <- RJSONIO::fromJSON(content_json)
+  content <- do.call("rbind", content)
+  return(content)
+}
