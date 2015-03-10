1886f92ba6104291087b44156e5226b8641b365d
Mike Skeen <skeen.mike@gmail.com>
2015-03-10 09:45:21 -0400

    Bug fix for datafiles_list

--- R/datafiles_functions.R
+++ R/datafiles_functions.R
@@ -11,7 +11,7 @@ datafiles_list <- function(api_key) {
   files_json <- RCurl::getURL(paste0("https://datashift.ca/api/v1/datafiles.json?api_key=", api_key))
   df_list <- RJSONIO::fromJSON(files_json)
   df_list <- do.call("rbind", df_list)
-  df_list <- data.frame(content)
+  df_list <- data.frame(df_list)
   return(df_list)
 }
 
