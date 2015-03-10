8a0c33b7ce41baeca2e72f318e3a28cb9343655b
Mike Skeen <skeen.mike@gmail.com>
2015-03-10 09:32:42 -0400

    Use csv instead of json

--- R/datafiles_functions.R
+++ R/datafiles_functions.R
@@ -2,12 +2,12 @@ datafiles_list <- function(api_key) {
   files_json <- RCurl::getURL(paste0("https://datashift.ca/api/v1/datafiles.json?api_key=", api_key))
   df_list <- RJSONIO::fromJSON(files_json)
   df_list <- do.call("rbind", df_list)
+  df_list <- data.frame(content)
   return(df_list)
 }
 
 datafile_content <- function(api_key, datafile_id) {
-  content_json <- RCurl::getURL(paste0("https://datashift.ca/api/v1/datafiles/", datafile_id, ".json?api_key=", api_key))
-  content <- RJSONIO::fromJSON(content_json)
-  content <- do.call("rbind", content)
+  content_csv <- RCurl::getURL(paste0("https://datashift.ca/api/v1/datafiles/", datafile_id, ".csv?api_key=", api_key))
+  content <- read.csv(text=content_csv)
   return(content)
 }
